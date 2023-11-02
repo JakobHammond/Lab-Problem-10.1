36deeebcb13853a7dcb8507676464947ce7c225e
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-11-02 14:30:51 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  p10-1
+//  p10-4.2
 //
 //  Created by Jakob Hammond on 11/2/23.
 //
@@ -8,21 +8,82 @@
 #include <iostream>
 using namespace std;
 
-int count_letters(string str);
+bool isCardValid(int digits[], int size);
+bool isNumeric(string s);
+bool isCorrectLength(string s);
 int main(void){
+    int digits[16];
+    int size = 16;
+    int number;
+    string numString;
     
-    cout << count_letters("I don't give a Holstein for programmers that can only spell a variable one way. (Dr. Stephany)") << endl;
+    
+    while(true){
+        cout << "Enter 16-digit credit card # or Q to quit: ";
+        
+  
+        cin >> numString;
+        if(numString == "Q") break;
+        
+        if(isNumeric(numString)){
+            if(isCorrectLength(numString)){
+                for(int i = 0; i <= size; i++){
+                    string first = numString.substr(numString.length() - i);
+                    digits[i] = first.front() - 48;
+                    
+                }
+                
+            }
+            else{
+                cout << "Error - card number must contain 16 digits." << endl;
+                continue;
+            }
+        }
+        else{
+            cout << "Error - card number must contain only digits." << endl;
+            continue;
+        }
+        
+        
+        
+       // for(int i = 0; i < size; i++){
+       //     digits[size - i] = number % 10;
+       //     number /= 10;
+       // }
+        
+        if(isCardValid(digits, size)) cout << "Card is valid." << endl;
+        else cout << "Card is not valid." << endl;
+    
+    }
+    return 1;
 }
 
-int count_letters(string str){
-    int letters = 0;
-    for(int i = 0; i <= str.length(); i++){
-        string first;
-        first = str.substr(str.length() - i);
-        if(first.front() <= 57);
-        else if((first.front() <= 90 && first.front() >= 49) || (first.front() <= 122 && first.front() >= 81)){
-            letters++;
-        }
+
+bool isCardValid(int digits[], int size){
+    int firstSum = 0;
+    int secondSum = 0;
+    for(int i = size - 1; i > 0; i -= 2){
+        firstSum += digits[i];
     }
-    return letters;
+    for(int i = size; i > 0; i -= 2){
+        int temp = digits[i] * 2;
+        secondSum += temp / 10;
+        secondSum += temp % 10;
+    }
+    if((firstSum + secondSum) % 10 == 0) return true;
+    else return false;
+}
+
+bool isNumeric(string s){
+    for(int i = 1; i < s.length(); i++){
+        string first;
+        first = s.substr(s.length() - i);
+        if(first.front() <= 47 || first.front() >= 58) return false;
+    }
+    return true;
+}
+
+bool isCorrectLength(string s){
+    if(s.length() == 16) return true;
+    else return false;
 }
